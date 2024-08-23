6c9def6dc92bc2e4e8717dfbde5f81083208d107
Joyce aboui <venueaboui@gmail.com>
2024-08-23 17:01:30 +0100

    first step

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,30 @@
+import { useState } from "react"
+
 const Home = () => {
+    // let name = 'mario';
+const [name , setName] = useState('Mario');
+const [age , setAge] = useState('25');
 
+
+    // const handleClick=() =>{
+    //     console.log('hello, ninjas');
+    // }
     const handleClick=() =>{
-        console.log('hello, ninjas');
+        setName('Luigi')
+        setAge('41')
+        // setName('John')
     }
 
-    const handleClickAgain= (name) =>{
-        console.log('hello' + name);
-    }
+    // const handleClickAgain= (name) =>{
+    //     console.log('hello' + name);
+    // }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <br />
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click Here</button>
-            <button onClick={() => handleClickAgain('Jenny') }>Click me again</button>
+            {/* <button onClick={() => handleClickAgain('Jenny') }>Click me again</button> */}
         </div>
      );
 }
--- src/navbar.js
+++ src/navbar.js
@@ -2,9 +2,14 @@ const  Navbar = () => {
     return ( 
 
         <nav className="navbar">
-            <h1>The TUTO-REACT blog </h1>
+            <h1>The REACT blog </h1>
             <div className="links">
-                <a href="/">Home</a>
+                <a href="/" style={{
+                    color:"White",
+                    backgroundColor:'black',
+                    borderRadius:'8px'
+                }}>Home</a>
+                
                 <a href="/create" style={{
                     color:"White",
                     backgroundColor:'black',
