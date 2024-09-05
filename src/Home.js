643b3b3107071a5c2c5c9edac17a7235016b3e98
Joyce aboui <venueaboui@gmail.com>
2024-09-05 16:58:00 +0100

    continuation of the profile page and adding hompage

--- src/Home.js
+++ src/Home.js
@@ -4,48 +4,43 @@ import usefetch from "./usefetch";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
-    const { data: blogs ,isPending, error ,setData: setBlogs} = usefetch('http://localhost:8000/blogs')
-    // const [blogs , setBlogs] = useState(null);
-    const [data , setData] = useState(null);
-    const [name , setName] = useState('Mario');
-    const [age , setAge] = useState('25');
-  
-
-// valeurs initiales 
-    const handleClick=() =>{
-        setName('Luigi')
-        setAge('41') 
-    }
-// bouton changeant
-    const handleClickAgain= (name) =>{
-        console.log('hello' + name);
-    }
-    const handleDelete = (id) =>{
-        const newBlogs = blogs.filter(blog => blog.id !== id )
-        setBlogs(newBlogs)
-
-    }
-// Fetching data from the DB
-
-    console.log('useEffect worked')
-    console.log(blogs)
+    const { data: blogs ,isPending, error ,setData: setBlogs} = usefetch('http://localhost:8000/courses')
+const[courses,setCourses]=useState()
+useEffect(() => {
+    fetch('http://localhost:8000/courses')
+    .then(res => {
+        return res.json()
+})
+.then(data => {
+    console.log(data)
+    setCourses(data)
+})
+console.log('ok')
+console.log(courses)
+});
 
     return ( 
-        <div className="home">
-            {error && <div>{error}</div>} 
-            {isPending && <div>Loading......</div>}
-            {blogs && <Bloglist  blogs={blogs} title='Courses' handleDelete={handleDelete}/> }
+        <div className="mainhome">
+            {/* COURSES DIV  */}
+            <h2 className="fw-bold">Courses</h2>
             <br />
-            {/* filter blogs */}
-            {blogs && <Bloglist blogs={blogs.filter((blog) => blog.author ==='Josiane')} title='Now Reading ' supervisor={'Maka Maka'}/> } 
+            <div className="courses-container">
+                {courses && courses.map((course) => (
+                    <div className="coursebox" key={course.id}>
+                        {course.name}
+                    </div>
+                ))}
+            </div>
 
-
-        {/* Result des boutons de la phrase  */}
-             {/* <h2>Homepage</h2>
+            {/* NOW READING DIV */}
             <br />
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Here</button>
-            <button onClick={() => handleClickAgain('Jenny') }>Click me again</button>  */}
+            <br />
+             <div className="nowreading">
+                        <h2 className="fw-bold">Now Reading</h2>
+            </div>
+            
+
+      
         </div> 
      );
 }
